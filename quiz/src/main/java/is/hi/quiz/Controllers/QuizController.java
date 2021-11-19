0cfc44ffcf0f45b00dc981ab53b0b56950c5b1b4
tommijons <toj6@hi.is>
2021-11-19 11:28:35 +0000

    display answers

--- quiz/src/main/java/is/hi/quiz/Controllers/QuizController.java
+++ quiz/src/main/java/is/hi/quiz/Controllers/QuizController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 import java.awt.*;
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -21,6 +22,8 @@ public class QuizController {
     private QuizService quizService;
     private AccountService as;
     private AccountController ac;
+    private List<String> answers = new ArrayList<>();
+    private List<String> correctAnswers= new ArrayList<>();
 
     @Autowired
     public QuizController(QuizService quizService, AccountService as,AccountController ac){
@@ -33,6 +36,8 @@ public class QuizController {
         Question nextQuestion;
         nextQuestion = getNextQuestion(id);
         model.addAttribute("questions", nextQuestion);
+        model.addAttribute("answers", answers);
+        model.addAttribute("correctanswers", correctAnswers);
         return "displayQuestion";
     }
 
@@ -41,6 +46,8 @@ public class QuizController {
         Quiz quiz= quizService.getQuiz((int)id,1);
         List<Question> allQuestions = quiz.getCategory().getQuestions();
         String questionAnswer = allQuestions.get(quizService.getNoOfQuestions()-1).getCorrectAnswer();
+        answers.add(option);
+        correctAnswers.add(questionAnswer);
         if(questionAnswer.equals(option)){
             quizService.addScore(100);
             System.out.println("CORRECT: "+" scores: "+quizService.getScore());
@@ -71,6 +78,7 @@ public class QuizController {
             return question;
         }
         quizService.saveScores(score);
+
         return null;
     }
 
